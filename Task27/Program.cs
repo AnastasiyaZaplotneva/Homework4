75bd5b48b03659c3c57b646efdc6f33d951659fc
Anastasiya Z <nastya_tim2201@mail.ru>
2022-11-08 14:57:11 +0300

    решили вторую задачу

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DigitSum (int num)
+{
+    int result = 0; //на случай, если пользователь введет 0, сумма будет 0
+    while(num != 0)
+    {
+        result = result + num%10;
+        num = num/10;
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summa = DigitSum(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {summa}");
+
